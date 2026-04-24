2ba2f1ac631b59607a874ff762ebd0551c45ee0b
RileyMwork <rileywmartin117@gmail.com>
2026-04-23 22:24:49 -0400

    none

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from edgar.controller.edgar_api_controller import EdgarApiController
 
 edgar_api_controller = EdgarApiController()
 info = edgar_api_controller.get_most_recent_transactions()
-# print(info)
+print(info)
