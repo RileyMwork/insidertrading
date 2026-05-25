9f0ce6c9371999664699ecd5fb7cda7521c8ce22
RileyMwork <rileywmartin117@gmail.com>
2026-05-25 19:20:06 -0400

    basic main file change

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 # ---------------- EDGAR -----------------
 
-# from edgar.controller.edgar_api_controller import EdgarApiController
-# import pandas as pd
+from edgar.controller.edgar_api_controller import EdgarApiController
+import pandas as pd
 
-# edgar_api_controller = EdgarApiController()
-# info = edgar_api_controller.get_most_recent_transactions()
-
-# df = edgar_api_controller.get_todays_transactions_df()
-# print(df["industry"].value_counts())
+edgar_api_controller = EdgarApiController()
+info = edgar_api_controller.get_most_recent_transactions()
 
 # ---------------- EDGAR -----------------
 
