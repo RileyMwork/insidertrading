4962432f8d874513cf7d1d0392d41f2daf6bb7ec
RileyMwork <rileywmartin117@gmail.com>
2026-07-01 17:44:41 -0400

    updated gitignore

--- components/alpaca_/alpaca_main.py
+++
@@ -1,6 +0,0 @@
-from alpaca_.service.alpaca_service import AlpacaService
-
-alpaca_service = AlpacaService()
-
-intervals = alpaca_service.generate_intervals("2024-01-16")
-print(intervals)
--- components/alpaca_/api/trading/account/alpaca_account_base.py
+++
@@ -1,9 +0,0 @@
-from components.alpaca_.api.alpaca_api_base import AlpacaApiBase
-
-class AlpacaAccountBase(AlpacaApiBase):
-    def __init__(self):
-        super().__init__()
-        
-    def get_account(self):
-        account = self.trading_client.get_account()
-        return account
