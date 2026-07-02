bb1e62c84e8e679ba970a14c5668361eb4f7cd22
RileyMwork <rileywmartin117@gmail.com>
2026-07-01 21:08:07 -0400

    updated repo components

--- main/main.py
+++ main/main.py
@@ -1,7 +1,7 @@
-from components.edgar.controller.edgar_controller import EdgarController
+from main.components.edgar.controller.edgar_controller import EdgarController
 
 edgar_controller = EdgarController()
 
-edgar_controller.get_transactions(start_date="2023-02-04", end_date="2023-02-06")
+edgar_controller.get_transactions(start_date="2023-02-07", end_date="2023-02-08")
 
 
